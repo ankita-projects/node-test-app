478d03c1a095a1c7f616d1ae2bd0c942d26c0656
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-08-19 14:58:32 +0200

    first commit

---
+++ client/src/BeachTemp/BeachTempratureCard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import { makeStyles } from "@material-ui/core/styles";
+import "./BeachTemprature.css";
+
+// location icon //
+const useStyles = makeStyles((theme) => ({
+  clickableIcon: {
+    color: "black",
+    "&:hover": {
+      color: "blue",
+    },
+  },
+}));
+// beach temprature card component which has 
+const BeachTempratureCard = ({ beachTemprature }) => {
+  const classes = useStyles();
+  return (
+    <Card className="card-beaches " key={beachTemprature.id}>
+      <Card.Img
+        className="card-img-top-beach"
+        variant="top"
+        src={beachTemprature.image}
+        alt={beachTemprature.name}
+      />
+      <Card.Body>
+        <Card.Title className= {classes.clickableIcon} onClick={() => window.open(beachTemprature.beachLocation)}>
+          <span>
+            <LocationOnIcon
+              onClick={() => window.open(beachTemprature.beachLocation)}
+              className={classes.clickableIcon}
+            />
+          </span>
+          {beachTemprature.beachName}
+        </Card.Title>
+        <Card.Text
+          className="card-text-beach"
+          style={
+            beachTemprature.waterTemp > 10
+              ? { backgroundColor: "orange" }
+              : { backgroundColor: "#42b6f5" }
+          }
+        >
+          {"Water temprature is " + beachTemprature.waterTemp + "°C "}
+          <br></br>
+          {"Air temprature is " + beachTemprature.airTemp + "°C  "}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BeachTempratureCard;
---
+++ client/src/BeachTemp/Beaches.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BeachTempratureCard from "./BeachTempratureCard";
+import CityWeatherCard from "./CityWeatherCard";
+import { Row } from "react-bootstrap";
+import logo from "./images/bicker.svg";
+let masterListOfBeaches;
+
+// Beaches component
+const Beaches = () => {
+  const [beaches, setBeaches] = useState([]);
+  const [cities, setCities] = useState([]);
+  function searchBeach(event) {
+    if (event.target.value !== "") {
+      setBeaches(
+        masterListOfBeaches.filter((x) =>
+          x.beachName.toLocaleLowerCase().startsWith(event.target.value.toLocaleLowerCase())
+        )
+      );
+    } else {
+      setBeaches(masterListOfBeaches);
+    }
+  }
+
+  const fetchData = async () => {
+    const beachesResponse = await axios(
+      "https://iknow-backend.herokuapp.com/beachTemp"
+    );
+    const citiesResponse = await axios(
+      "https://iknow-backend.herokuapp.com/weather/"
+    );
+    masterListOfBeaches = beachesResponse.data;
+    setBeaches(beachesResponse.data);
+    setCities(citiesResponse.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedResult = (
+    <div className="beachContainer">
+      <Row className="cityWeatherCards">
+        <img src={logo} alt="logo" />
+        {cities.map((cityWeather) => (
+          <CityWeatherCard key={cityWeather.id} cityWeather={cityWeather} />
+        ))}
+      </Row>
+   
+      <Row>
+        {beaches.map((beachTemp) => (
+          <BeachTempratureCard key={beachTemp.id} beachTemprature={beachTemp} />
+        ))}
+      </Row>
+    </div>
+  );
+  return renderedResult;
+};
+
+export default Beaches;
